bb6a8479d08b2f428c653dbfa0329e574b2ebec3
Prajaktabhosale306 <Prajaktabhosale306@gmail.com>
2026-06-25 13:07:57 +0530

    removed secrets

--- api/auth_api.py
+++ api/auth_api.py
@@ -1,11 +1,12 @@
+import os
 from .base_client import BaseClient
 
 class AuthAPI(BaseClient):
     def create_token(self, username, password):
         endpoint = "/auth"
         data = {
-            "username": username,
-            "password": password
+            "username": os.getenv("API_USERNAME"),
+            "password": os.getenv("API_PASSWORD")
         }
         response = self.post(endpoint, json_data=data)
         return response
