5a05b517b945642fee6826f685d163ef9ef01425
Prajaktabhosale306 <Prajaktabhosale306@gmail.com>
2026-06-28 21:44:33 +0530

    failure analysis plugin

--- ai/providers/gemini_provider.py
+++ ai/providers/gemini_provider.py
@@ -9,13 +9,14 @@ class GeminiProvider:
     """Google Gemini Provider"""
 
     def __init__(self, model="gemini-2.5-flash"):
+        import google.generativeai as genai
         api_key = os.getenv("GEMINI_API_KEY")
 
         if not api_key:
-            raise ValueError("GEMINI_API_KEY not found.")
+            raise Exception("GEMINI_API_KEY not found.")
 
-        self.client = genai.Client(api_key=api_key)
-        self.model = model
+        genai.configure(api_key=api_key)
+        self.model = genai.GenerativeModel(model)
 
     def ask(self, prompt, max_tokens=1024):
         try:
